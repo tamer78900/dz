0c534d59d2981e18c5b31459e721fc337573f934
tamer78900 <114273077+tamer78900@users.noreply.github.com>
2023-03-04 09:58:08 +0600

    Initial commit

---
+++ myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db import models
+
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
